0771c18ab669dfab30666ffb30cb4e68c8e0b43f
indiraprofessor74 <166089311+indiraprofessor74@users.noreply.github.com>
2026-02-11 10:40:12 +0530

    save

--- weatherapp/myapp/src/main.jsx
+++ weatherapp/myapp/src/main.jsx
@@ -2,10 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Useeffect1 from './Useeffect1.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Useeffect1 />
+    <App />
   </StrictMode>,
 )
